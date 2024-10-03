a5e813b9bd8ade0871222fb0a754110bce110ca1
Ajjack4 <manavjadhav1997@gmail.com>
2024-10-03 19:36:44 +0530

    final version

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,11 +2,7 @@ class PatientsController < ApplicationController
     def index
       @patients = Patient.all
     end
-    def destroy
-        @patient = Patient.find(params[:id])
-        @patient.destroy
-        redirect_to patients_path, notice: 'Patient was successfully deleted.'
-      end
+  
     def new
       @patient = Patient.new
     end
@@ -26,14 +22,11 @@ class PatientsController < ApplicationController
       @patient = Patient.find(params[:id])
     end
   
-    # def update
-    #   @patient = Patient.find(params[:id])
-    #   if @patient.update(patient_params)
-    #     redirect_to patients_path, notice: 'Patient was successfully updated.'
-    #   else
-    #     render :edit
-    #   end
-    # end
+    def destroy
+        @patient = Patient.find(params[:id])
+        @patient.destroy
+        redirect_to patients_path, notice: 'Patient was successfully deleted.'
+      end
     def update
         @patient = Patient.find(params[:id])
         if @patient.update(patient_params)
